99a8164c504ae28c2aa2504ae68d9e06112cba19
Wojciech Szućko <wojciech.szucko@gmail.com>
2020-02-08 17:16:23 +0100

    Add react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { client } from './core/graphql/client';
 
+const HomeView = lazy(() => import('./views/HomeView/HomeView'));
+const DetailsView = lazy(() => import('./views/DetailsView/DetailsView'));
+
 const App = () => {
 	return (
 		<ApolloProvider client={client}>
-			<div className="App">
-
-			</div>
+			<BrowserRouter>
+				<Suspense fallback={<>Loading</>}>
+					<Switch>
+						<Route path="/:pokemon">
+							<DetailsView/>
+						</Route>
+						<Route path="/">
+							<HomeView/>
+						</Route>
+					</Switch>
+				</Suspense>
+			</BrowserRouter>
 		</ApolloProvider>
 	);
-}
+};
 
 export default App;
---
+++ src/views/DetailsView/DetailsView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DetailsView: React.FC = props => {
+	return (
+		<div>DetailsView works!</div>
+	);
+};
+
+export default DetailsView;
